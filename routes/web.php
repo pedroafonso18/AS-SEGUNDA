fa9b6393ebeeff78aba19649b5ed9916dc6e8572
Pedro Afonso <157052926+pedroafonso18@users.noreply.github.com>
2024-12-01 20:12:48 -0300

    Criadas as rotas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PessoaController;
+use App\Http\Controllers\CarroController;
+use App\Http\Controllers\AnimalController;
+use App\Http\Controllers\LivroController;
+use App\Http\Controllers\EmpresaController;
+use App\Http\Controllers\PedidoController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('pessoas', PessoaController::class);
+Route::resource('carros', CarroController::class);
+Route::resource('animais', AnimalController::class);
+Route::resource('livros', LivroController::class);
+Route::resource('empresas', EmpresaController::class);
+Route::resource('pedidos', PedidoController::class);
